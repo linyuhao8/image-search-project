f6610aeba4dd7143c130cf5896bd99354100c132
hank <linhao8896@gmail.com>
2025-01-28 15:10:08 +0800

    initial structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // 其他配置
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**", // 允許所有路徑
+      },
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/components/ImageGrid.js
@@ -0,0 +1,4 @@
+import Picture from "./Picture";
+export default function Grid() {
+  return <div className="grid grid-cols-4 gap-4"></div>;
+}
